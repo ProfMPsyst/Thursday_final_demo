bbc907861cc26a99d53c2a7fd88cde27ed046225
megha <megha.patel2@sheridancollege.ca>
2023-01-19 15:16:41 -0500

    first commit

---
+++ src/gitdemo/GitDemo.java
@@ -0,0 +1,17 @@
+
+package gitdemo;
+
+/**
+ *
+ * @author Megha Patel
+ */
+public class GitDemo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("This is Git Demo");
+    }
+
+}
